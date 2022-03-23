29c860866f1d646408c1b23071f2e7b1675ee7fd
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-23 09:44:14 +0000

    Removed pointless buffer flush commands

--- csgo/run_steamcmd.sh
+++ csgo/run_steamcmd.sh
@@ -9,9 +9,9 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>$logfile 2>&1
 # Logging config end
 echo "Updating steamcmd."
-stdbuf -oL ./steamcmd.sh +app_update +quit
+./steamcmd.sh +app_update +quit
 echo "Installing/configuring the game server."
-stdbuf -oL ./steamcmd.sh +force_install_dir ./data +login anonymous +app_update $game_id +quit
+./steamcmd.sh +force_install_dir ./data +login anonymous +app_update $game_id +quit
 echo "Adding server ready file, for hand back"
 echo "READY" > ./data/server.state
 echo "Processing complete."
