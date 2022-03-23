2cfa57bdb7276e58122d4cb842665afcbc3b9565
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-23 09:47:04 +0000

    Added states to server.state

--- csgo/run_steamcmd.sh
+++ csgo/run_steamcmd.sh
@@ -8,8 +8,10 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>$logfile 2>&1
 # Logging config end
+echo "UPDATING STEAMCMD" > ./data/server.state
 echo "Updating steamcmd."
 ./steamcmd.sh +app_update +quit
+echo "PULLING SERVER FILES" > ./data/server.state
 echo "Installing/configuring the game server."
 ./steamcmd.sh +force_install_dir ./data +login anonymous +app_update $game_id +quit
 echo "Adding server ready file, for hand back"
