3d79a47dd5fe7ddd4ebc3d4172d0c9b75909e434
root <root@node3864-dockerdev.za.cloudlet.cloud>
2022-03-23 17:29:38 +0000

    Cleaning

--- csgo/start.sh
+++ csgo/start.sh
@@ -3,6 +3,4 @@ export LD_LIBRARY_PATH=/home/steam/steamcmd/data:/home/steam/steamcmd/data/bin
 
 # https://developer.valvesoftware.com/wiki/Counter-Strike:_Global_Offensive_Dedicated_Servers#Docker
 
-# Instructions for http://steamcommunity.com/dev/managegameservers
-
 # srcds -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount [STEAM_APP_ID http://steamcommunity.com/dev/managegameservers]
