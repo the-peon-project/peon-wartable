cc93ed89e09394c5874cab6ea5b8588c7e5a262a
Umlatt <theplacetosendmemail@gmail.com>
2022-04-06 20:20:02 +0000

    Better logging

--- games/csgo/start.sh
+++ games/csgo/start.sh
@@ -13,19 +13,19 @@ fi
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>$logfile 2>&1
-echo "############################## WAITING FOR UPDATE TO COMPLETE ##############################"
+echo "################## WAITING FOR SERVER FILES ##################"
 timeout=0
 while [ -z $(grep -P  "Success! App .* fully installed." /var/log/peon/update.sh.log) ]; do
     ((timeout++))
     printf "."
     sleep 1
-    if (( $timeout >= 600 )); then 
+    if (( $timeout >= 1200 )); then 
         printf "\nThe start script timedout after 10 minutes."
         exit 124
     fi
 done
 printf "\nUpdate took $timeout seconds.\n"
-echo "############################## SERVER IS READY ##############################"
+echo "############### SERVER FILES AVAILABLE READY ################"
 printf "\nStarting game server."
-data/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount $steam_app_id &
-echo "RUNNING" > ./data/server.state 
+data/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount $steam_app_id
+echo "RUNNING" > ./data/server.state
--- shared/steamcmd/update.sh
+++ shared/steamcmd/update.sh
@@ -15,10 +15,13 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>$logfile 2>&1
 # Logging config end
+echo "##################### UPDATING STEAMCMD ######################"
 echo "STEAMCMD UPDATING" > ./data/server.state
 ./steamcmd.sh +app_update +quit
-echo "############################## STEAMCMD UPDATED ##############################"
+echo "###################### STEAMCMD UPDATED ######################"
+echo "################## DOWNLOADING GAME SERVER ###################"
+echo "SERVER [$game_id]"
 echo "GAME SERVER UPDATING" > ./data/server.state
 ./steamcmd.sh +force_install_dir ./data +login anonymous +app_update $game_id +quit
-echo "############################## GAME SERVER UPDATED ##############################"
+echo "#################### GAME SERVER UPDATED #####################"
 echo "READY" > ./data/server.state
