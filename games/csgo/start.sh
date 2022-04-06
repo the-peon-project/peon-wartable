0510278d0019cb52cdd89bd7bfaf0beda617e3d0
Umlatt <theplacetosendmemail@gmail.com>
2022-04-06 15:36:07 +0000

    Added update wait

--- games/csgo/start.sh
+++ games/csgo/start.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/home/steam/steamcmd/data:/home/steam/steamcmd/data/bin
-
-# https://developer.valvesoftware.com/wiki/Counter-Strike:_Global_Offensive_Dedicated_Servers#Docker
-
-# ./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount [STEAM_APP_ID http://steamcommunity.com/dev/managegameservers]
+logfile="/var/log/peon/${0##*/}.log"
+# Handle parameters
+if [ $# -eq 0 ]; then
+    echo "Steam App ID required. e.g. > ./${0##*/} 12345"
+    echo "Script was run without a steam App ID" >>$logfile
+    exit 1
+else
+    steam_app_id=$1
+fi
+# Logging config start - capture all
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>>$logfile 2>&1
+printf "Waiting for update to complete\n"
+timeout=0
+while [[ $(cat data/server.state) != "READY" ]]; do
+    ((timeout++))
+    printf "."
+    sleep 1
+    if (( $timeout >= 600 )); then 
+        printf "\nThe update script did not complete within 10 minutes."
+        exit 124
+    fi
+done
+printf "\nUpdate took $timeout seconds.\n"
+printf "Starting game server."
+data/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount $steam_app_id &
