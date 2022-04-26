6375d4ac3bed97449ae011b72abec93b1d10a28c
Umlatt <theplacetosendmemail@gmail.com>
2022-04-26 18:05:52 +0000

    Initial commit

---
+++ games/valhiem/server_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+logfile="/var/log/peon/${0##*/}.log"
+rootpath="/home/steam/steamcmd/"
+cd $rootpath
+echo "" > $logfile
+# Logging config start - capture all
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>>$logfile 2>&1
+# Logging config end
+echo "##################### STARTING SERVER ######################"
+# CUSTOM GAME SERVER COMMAND
+./valheim_server.x86_64 -name $SERVERNAME -port 2456 <-nographics> <-batchmode> -world $WORLDNAME -password $PASSWORD -public 1 >> $logfile < /dev/null & 
