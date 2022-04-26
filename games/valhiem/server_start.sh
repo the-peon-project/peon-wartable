b132639e923c5114dd80243228bdea254855665e
Umlatt <theplacetosendmemail@gmail.com>
2022-04-26 20:39:40 +0000

    Working v1 - no customisation

--- games/valhiem/server_start.sh
+++ games/valhiem/server_start.sh
@@ -10,4 +10,4 @@ exec 1>>$logfile 2>&1
 # Logging config end
 echo "##################### STARTING SERVER ######################"
 # CUSTOM GAME SERVER COMMAND
-./valheim_server.x86_64 -name $SERVERNAME -port 2456 -world $WORLDNAME -password $PASSWORD -public 1 >> $logfile < /dev/null & 
+cd data && ./valheim_server.x86_64 -name $SERVERNAME -port 2456 -world $WORLDNAME -password $PASSWORD -public 1
