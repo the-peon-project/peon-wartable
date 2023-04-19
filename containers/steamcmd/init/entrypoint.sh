ca2f979911d191aabdcd4b85099114e81a28fe60
umlatt <theplacetosendmemail@gmail.com>
2023-04-19 22:23:41 +0000

    Reverted to root user

--- containers/steamcmd/init/entrypoint.sh
+++ containers/steamcmd/init/entrypoint.sh
@@ -16,18 +16,15 @@ then
     /init/init_custom && echo "CUSTOM INIT - COMPLETE" || { echo "CUSTOM INIT - FAILED"; exit 1; }
 fi
 echo "##### Initialisation [END]"
-# CONFIGURE PERMISSIONS
-chown -R 1000:1000 /home
 ### SERVER START
 echo "##### Server [START]"
 if [ -e "/init/server_start" ]
 then
     echo -n "READY" > $state_file
     dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '\"' > $ip_file
-    su - steam -c "/init/server_start"
+    /init/server_start
 else
     echo -n "ERROR - BAD PLAN" > $state_file
     echo " ERROR /init/server_start.sh not found. Did you forget to mount it?"
 fi
 echo "##### Server [END]"
-echo ""
