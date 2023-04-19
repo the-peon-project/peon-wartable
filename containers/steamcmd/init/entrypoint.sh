cb3e04835b30f70fcda8e0e8ec6238947eb2fd03
umlatt <theplacetosendmemail@gmail.com>
2023-04-19 20:43:38 +0000

    + permissions, init_custom and steam user start

--- containers/steamcmd/init/entrypoint.sh
+++ containers/steamcmd/init/entrypoint.sh
@@ -11,19 +11,20 @@ echo -n "CONTAINER BOOTING" > $state_file
 # RUN STANDARD SCRIPT
 /init/init.sh && echo "PEON INIT - COMPLETE" || { echo "PEON INIT - FAILED"; exit 1; }
 # RUN CUSTOM INIT, IF FOUND
-if [ -e "/init/init_custom.sh" ]
+if [ -e "/init/init_custom" ]
 then
-    /init/init_custom.sh && echo "CUSTOM INIT - COMPLETE" || { echo "CUSTOM INIT - FAILED"; exit 1; }
+    /init/init_custom && echo "CUSTOM INIT - COMPLETE" || { echo "CUSTOM INIT - FAILED"; exit 1; }
 fi
 echo "##### Initialisation [END]"
-
+# CONFIGURE PERMISSIONS
+chown -R 1000:1000 /home
 ### SERVER START
 echo "##### Server [START]"
 if [ -e "/init/server_start" ]
 then
     echo -n "READY" > $state_file
     dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '\"' > $ip_file
-    /init/server_start
+    su - steam -c "/init/server_start"
 else
     echo -n "ERROR - BAD PLAN" > $state_file
     echo " ERROR /init/server_start.sh not found. Did you forget to mount it?"
